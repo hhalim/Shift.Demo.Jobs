6285ad39b73db00a770e2674e10ecb35fb6dac88
Hartono Halim <gopher@equalbyte.com>
2017-03-23 11:05:06 -0500

    Update to Shift 1.0.4.

--- TestJob.cs
+++ TestJob.cs
@@ -10,13 +10,18 @@ namespace Shift.Demo.Jobs
 {
     public class TestJob
     {
-        public void Start(string value, IProgress<ProgressInfo> progress)
+        public void Start(string value, IProgress<ProgressInfo> progress, CancellationToken token)
         {
             var total = 10;
 
             var note = "";
             for (var i = 0; i < total; i++)
             {
+                if (token.IsCancellationRequested)
+                {
+                    token.ThrowIfCancellationRequested(); //throw OperationCanceledException
+                }
+
                 note += i + " - " + value + "<br/> \n";
 
                 var pInfo = new ProgressInfo();
@@ -25,7 +30,7 @@ namespace Shift.Demo.Jobs
                 if (progress != null)
                     progress.Report(pInfo);
 
-                Thread.Sleep(3000);
+                Thread.Sleep(2500);
             }
 
             return;
